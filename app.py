a453c9c3a6297dcba16052cc70f802c764a5dcbc
SlidrusForeal <49024907+SlidrusForeal@users.noreply.github.com>
2025-04-13 12:38:28 +0300

    gi

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-# app.py
-from flask import Flask, render_template, request, redirect, url_for, abort, flash
-from blacklist import BLACKLIST, BLACKLIST_DATA
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for, abort, flash, session, jsonify
 from datetime import datetime
+from functools import wraps
+from sqlalchemy import func
+from blacklist import BLACKLIST_DATA, BLACKLIST
+
+load_dotenv()  # Загружаем переменные окружения из .env
 
 app = Flask(__name__)
-app.secret_key = "aYmi2Tfq1kbW-OlU8-r7cJO1p"  # required for flashing messages
+app.secret_key = os.environ.get("SECRET_KEY")
+if not app.secret_key:
+    raise ValueError("Не задан SECRET_KEY в переменных окружения!")
+
+# Конфигурация подключения к базе данных
+db_user = os.environ.get("MYSQL_USER")
+db_password = os.environ.get("MYSQL_PASSWORD")
+db_host = os.environ.get("MYSQL_HOST")
+db_name = os.environ.get("MYSQL_DB")
+
+if not all([db_user, db_password, db_host, db_name]):
+    raise ValueError("Не все параметры для подключения к базе данных заданы!")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route("/", methods=["GET", "POST"])
 def index():
